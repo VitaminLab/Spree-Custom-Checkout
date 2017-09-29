8d5407dc9c7b9497cb1133571ad3b92ec42316a0
Yaniv Ran <yran@appcolony.ca>
2017-09-28 22:07:00 -0700

    prepend route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
-Rails.application.routes.draw do
-  mount Spree::Core::Engine, :at => '/'
-  namespace :spree do
-    namespace :api, defaults: { format: 'json' } do
-      resources :complete_orders
-    end
+# Rails.application.routes.draw do
+#   mount Spree::Core::Engine, :at => '/'
+#   namespace :spree do
+#     namespace :api, defaults: { format: 'json' } do
+#       resources :complete_orders
+#     end
+#   end
+# end
+Spree::Core::Engine.routes.prepend do
+  namespace :api do
+    resources :complete_orders
   end
 end
